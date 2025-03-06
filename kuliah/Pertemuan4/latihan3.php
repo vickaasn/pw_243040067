f7d35e233e77381c303a6833243137eed771d84a
vickaasn <vickaas29@gmail.com>
2025-03-06 11:46:10 +0700

    mengerjakan tugas 2

--- kuliah/Pertemuan4/latihan3.php
+++ kuliah/Pertemuan4/latihan3.php
@@ -3,9 +3,8 @@
 $i = 1;
 for ($i = 1; $i <= 5; $i++) {
     for ($j = 1; $j <= 3; $j++) {
-        echo "$j";
+        echo "$j ";
     }
-
     echo "<br>";
 }
 
@@ -13,21 +12,25 @@ echo "<hr>";
 
 for ($i = 1; $i <= 2; $i++) {
     for ($j = 1; $j <= 6; $j++) {
-        echo "$j";
+        echo "$j ";
     }
-
     echo "<br>";
 }
 
 echo "<hr>";
 
-for ($i = 1; $i <=`  7; $i++) {
+for ($i = 1; $i <= 7; $i++) {
     for ($j = 1; $j <= 2; $j++) {
-        echo "$j";
+        echo "$j ";
     }
-
     echo "<br>";
 }
 
 echo "<hr>";
 
+for ($i = 1; $i <= 4; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "$i ";
+    }
+    echo "<br>";
+}
